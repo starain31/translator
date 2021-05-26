1ab13e4df8a1172771d3a932e23c6d8bea250da4
sakib <nsakib.cse@gmail.com>
2021-05-26 23:00:23 +0600

    First scenario passed.

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,13 +1,22 @@
 const {Then, When, Given} = require('@cucumber/cucumber');
+const {assertThat, is} = require('hamjest');
 
-Given('translate from {string} to {string} selected', function () {
-    return 'pending';
+function create_translator({source_language, target_language}) {
+    return Object.freeze({
+        translate: function (source_text) {
+            return 'ওহে';
+        }
+    });
+}
+
+Given('translate from {string} to {string} selected', function (source_language, target_language) {
+    this.translator = create_translator({source_language, target_language});
 });
 
-When('{string} is given to translate', function () {
-    return 'pending';
+When('{string} is given to translate', function (source_text) {
+    this.translated_text = this.translator.translate(source_text);
 });
 
-Then('translated message should be {string}', function () {
-    return 'pending';
+Then('translated message should be {string}', function (target_text) {
+    assertThat(this.translated_text, is(target_text));
 });
